fad9321ffc61f7aec659cc7f9f097849d5eca05c
Arsen Hevorkian <ahevorkian@cdtnue.onmicrosoft.com>
2026-06-29 14:26:23 +0200

    agregat_funktionen weiter

--- trainingdb_04_agregat_funktionen.sql
+++ trainingdb_04_agregat_funktionen.sql
@@ -2,3 +2,24 @@ SELECT *FROM Bestellungen;
 SELECT SUM(Betrag) FROM Bestellungen;
 SELECT COUNT(*) AS Anzahl FROM Bestellungen;
 SELECT COUNT(*) AS Anzahl FROM Bestellungen WHERE Betrag = 120;
+
+SELECT *FROM Kunden;
+SELECT *FROM Bestellungen;
+
+SELECT
+    k.Nachname,
+    COUNT(b.BestellungID) AS Anzahl_Bestellungen
+FROM Kunden k
+LEFT JOIN Bestellungen b
+ON k.KundenID = b.KundenID
+GROUP BY k.KundenID, k.Nachname;
+
+SELECT 
+    k.KundenID,
+    k.Vorname,
+    COUNT(b.BestellungID) AS Anzahl_Bestellungen
+FROM Kunden k
+LEFT JOIN Bestellungen b
+ON k.KundenID = b.KundenID
+GROUP BY k.KundenID, k.Vorname;
+
